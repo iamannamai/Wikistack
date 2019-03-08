6b497b92a7159d1bce40e193c1837c10faa5bea6
Anna Mai <annam0403@gmail.com>
2019-03-07 22:29:18 -0500

    Markup support

--- views/editPage.js
+++ views/editPage.js
@@ -1,5 +1,5 @@
-const html = require("html-template-tag");
-const layout = require("./layout");
+const html = require('html-template-tag');
+const layout = require('./layout');
 
 module.exports = (page, author) => layout(html`
   <h3>Edit a Page</h3>
--- views/wikipage.js
+++ views/wikipage.js
@@ -1,4 +1,4 @@
-const html = require("html-template-tag");
+const html = require('html-template-tag');
 const marked = require('marked');
 const layout = require('./layout');
 
@@ -8,7 +8,7 @@ module.exports = (page, author) => layout(html`
   </h3>
   <h4>by <a href="/users/${author.id}">${author.name}</a></h4>
   <hr/>
-  <div class="page-body">${page.content}</div>
+  <div class="page-body">$${marked(page.content)}</div>
   <hr/>
   <a href="/wiki/${page.slug}/edit" class="btn btn-primary">edit this page</a>
   <a href="/wiki/${page.slug}/delete" class="btn btn-danger">delete this page</a>
