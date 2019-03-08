f69a0c41283ac751d6c589cff65f432ae9fd4944
Anna Mai <annam0403@gmail.com>
2019-03-07 21:53:13 -0500

    Update users id page

--- Routes/wiki.js
+++ Routes/wiki.js
@@ -55,7 +55,7 @@ router.get('/:slug', async (req, res, next) => {
       res.status(404).send('404 Page Not Found');
     }
 
-    res.send(wikiPage(pageContent[0], pageContent[0].author.name));
+    res.send(wikiPage(pageContent[0], pageContent[0].author));
   } catch (error) {
     next(error);
   }
--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const html = require('html-template-tag');
-const layout = require('./views/layout');
 const main = require('./views/main.js');
-const { db, Page, User } = require('./models');
-const user = require('./Routes/user');
-const wiki = require('./Routes/wiki');
+const { db, Page } = require('./models');
 
 db.authenticate().
 then(() => {
@@ -16,7 +13,7 @@ then(() => {
 app.use(express.json());    //parse post requests with json body
 app.use(express.urlencoded({ extended: false }));   //parse post requests with urlencoded bodies
 app.use(morgan('dev'));
-app.use(express.static('Public'));
+app.use(express.static('public'));
 
 app.get('/', async (req, res, next) => {
     try {
@@ -25,16 +22,14 @@ app.get('/', async (req, res, next) => {
         <li>
             <a href="wiki/${page.slug}">${page.title}</a>
         </li>`);
-
         res.send(main(content));
-        //res.redirect('/wiki');
     } catch (error) {
         next(error);
     }
 });
 
-app.use('/users', user);
-app.use('/wiki', wiki);
+app.use('/users', require('./routes/user'));
+app.use('/wiki', require('./routes/wiki'));
 
 async function init() {
     await db.sync(
